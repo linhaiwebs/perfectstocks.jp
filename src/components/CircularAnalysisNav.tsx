56f521a0b37f12900944145f966955bf1d79b9d7
linhaiwebs <lhwebs@outlook.com>
2025-11-20 23:26:53 +0800

    Updated CircularAnalysisNav.tsx

--- src/components/CircularAnalysisNav.tsx
+++ src/components/CircularAnalysisNav.tsx
@@ -1,7 +1,3 @@
 export default function CircularAnalysisNav() {
-  return (
-    <div className="w-full mt-2">
-      <img src="/footer.png" alt="" className="w-full h-auto object-cover" />
-    </div>
-  );
+  return null;
 }
